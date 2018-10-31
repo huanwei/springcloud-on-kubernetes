b2955c80e8324acebe29a0b7070450fae9d6105b
huanwei <huan@harmonycloud.cn>
2018-10-31 17:30:12 +0800

    review
    Signed-off-by: huanwei <huan@harmonycloud.cn>

--- demo/springcloudserver/SpringCloudDemo/BookUser/src/main/java/cn/harmonycloud/springcloud/bookuser/BookuserApplication.java
+++ demo/springcloudserver/SpringCloudDemo/BookUser/src/main/java/cn/harmonycloud/springcloud/bookuser/BookuserApplication.java
@@ -4,16 +4,14 @@ import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServl
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
-@EnableEurekaClient
+@SpringCloudApplication
 @EnableFeignClients
-@EnableCircuitBreaker
 @EnableHystrixDashboard
 public class BookuserApplication {
     @Bean
