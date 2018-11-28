e77f3c23410724822d74ac1f29f6fb8bd818db30
huanwei <huan@harmonycloud.cn>
2018-11-28 16:32:32 +0800

    fix demo issue
    Signed-off-by: huanwei <huan@harmonycloud.cn>

--- demo/springcloudserver/zuulserver/fidelity/script/TesterFilter.groovy
+++ demo/springcloudserver/zuulserver/fidelity/script/TesterFilter.groovy
@@ -6,6 +6,7 @@ import com.alibaba.fastjson.JSONObject
 import com.netflix.config.ConfigurationManager
 import com.netflix.zuul.ZuulFilter
 import com.netflix.zuul.context.RequestContext
+import org.apache.commons.lang3.StringUtils
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
 
@@ -14,6 +15,8 @@ import javax.servlet.http.HttpServletResponse
 
 @Component
 class TesterFilter extends ZuulFilter {
+    String allowOrigin = '*'
+
     @Override
     String filterType() {
         return "pre"
@@ -32,40 +35,52 @@ class TesterFilter extends ZuulFilter {
     @Override
     Object run() {
         RequestContext context = RequestContext.currentContext
-        HttpServletRequest request = context.getRequest()
+        def request = context.getRequest()
+        def response = context.getResponse()
+
+        if (StringUtils.isNotBlank(allowOrigin)) {
+            String origin = allowOrigin;
+            String requestOrigin = request.getHeader("Origin");
+            if (StringUtils.isNotBlank(requestOrigin)
+                    && (allowOrigin.equals("*") || allowOrigin.indexOf(requestOrigin) > -1)) {
+                origin = requestOrigin;
+            }
+            response.setHeader("Access-Control-Allow-Origin", origin);
+            response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT, PATCH");
+            response.setHeader("Access-Control-Max-Age", "1200");
+            response.setHeader("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+        }
+
         JSONObject result = JSONObject.parseObject(new RestTemplate().getForObject("http://192.168.1.42:8761/eureka/apps/" + context.get("serviceId").toString() + "/", String.class))
         ConfigurationManager.getConfigInstance().setProperty(context.get("serviceId").toString() + ".ribbon.listOfServers", getServiceStr(result))
 
-        String uri = getServiceStr(result)
-        String s = context.getZuulRequestHeaders().get("x-forwarded-prefix");
-        String pix = context.getRequest().getRequestURI().split(s)[1];
+        String split = context.getZuulRequestHeaders().get("x-forwarded-prefix")
+        String pix = context.getRequest().getRequestURI().split(split)[1]
 
         String url = "http://" + LoadbalancerFactory.get(context.get("serviceId").toString()) + pix
         println(url)
 
-        HttpServletResponse response = this.httpRequest(url, this.getVerb(context.getRequest()), context.getRequest().getParameterMap(), context.getResponse())
+        HttpServletResponse resp = this.httpRequest(url, getVerb(context.getRequest()), context.getRequest().getParameterMap(), context.getResponse())
         context.setSendZuulResponse(false)
 
-//        String a = new RestTemplate().getForObject(uri, String.class)
-//        context.setResponseBody(a+":testzuul")
-//        context.setSendZuulResponse(false)
-        return response
+        return resp
     }
 
-    String getServiceStr(JSONObject obj) {
-        StringBuffer sb = new StringBuffer();
+    static String getServiceStr(JSONObject obj) {
+        StringBuffer sb = new StringBuffer()
 
-        JSONArray resultList = obj.getJSONObject("application").getJSONArray("instance");
+        JSONArray resultList = obj.getJSONObject("application").getJSONArray("instance")
         for (Object rs : resultList) {
-            JSONObject instaceService = (JSONObject) rs;
+            JSONObject instaceService = (JSONObject) rs
             if (sb.length() != 0) {
-                sb.append(",");
+                sb.append(",")
             }
-            String ipAddress = instaceService.getString("ipAddr");
-            sb.append(ipAddress + ":" + instaceService.getJSONObject("port").getString("\$"));
+            String ipAddress = instaceService.getString("ipAddr")
+            sb.append(ipAddress + ":" + instaceService.getJSONObject("port").getString("\$"))
         }
 
-        return sb.toString();
+        return sb.toString()
     }
 
     HttpServletResponse httpRequest(String requestUrl,
@@ -79,35 +94,33 @@ class TesterFilter extends ZuulFilter {
             conn.setDoInput(true)
             conn.setRequestMethod(requestMethod)
             conn.setRequestProperty("content-type", "text/html")
-
             // 使用输出流添加数据至请求体
             if (requestParameters.size() != 0) {
                 String json = JSON.toJSON(requestParameters)
-                OutputStream os = conn.getOutputStream();//获取输出流
+                OutputStream os = conn.getOutputStream()//获取输出流
                 os.write(json.getBytes())//写入
                 os.flush()
             }
-            conn.connect(); // 读取服务器端返回的内容
-            InputStream is = conn.getInputStream();
-            InputStreamReader isr = new InputStreamReader(is, "utf-8");
-            BufferedReader br = new BufferedReader(isr);
-            String line;
+            conn.connect() // 读取服务器端返回的内容
+            InputStream is = conn.getInputStream()
+            InputStreamReader isr = new InputStreamReader(is, "utf-8")
+            BufferedReader br = new BufferedReader(isr)
+            String line
             StringBuffer sb = new StringBuffer()
 
             while ((line = br.readLine()) != null) {
                 sb.append(line)
             }
-
             response.getWriter().write(sb.toString())
-
-            return response;
+            return response
         } catch (Exception e) {
-            logger.error("http request exception", e); throw e;
+            logger.error("http request exception (zuul)", e)
+            throw e
         }
     }
 
-    String getVerb(HttpServletRequest request) {
-        String method = request.getMethod();
-        return method == null ? "GET" : method;
+    static String getVerb(HttpServletRequest request) {
+        String method = request.getMethod()
+        return method == null ? "GET" : method
     }
 }
